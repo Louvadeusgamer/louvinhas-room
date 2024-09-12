ee971b4a15f1e374ea951963d9c72bd89ae1c577
LouvinhaUwU <103537728+Louvadeusgamer@users.noreply.github.com>
2024-09-12 18:56:04 -0300

    a

--- colapseScript.js
+++ colapseScript.js
@@ -1,22 +1,29 @@
 const items = {
     i0: {
-        2024: document.getElementById("0-2024"),
-        2023: document.getElementById("0-2023")
+        y2024: document.getElementById("0-2024"),
+        y2023: document.getElementById("0-2023")
     }
 }
 let showing = {
     i0: {
-        2024: true, 
-        2023: false
+        y2024: true, 
+        y2023: false
     }
 }
 
 function colapse(item, year) {
-    if (showing[item].year) {
+    if (year === 2024 && showing[item].y2024) {
         items[item].y2024.classList.replace("show", "hide")
-        showing[item].year = false
-    } else {
+        showing[item].y2024 = false
+    } else if (year === 2024) {
         items[item].y2024.classList.replace("hide", "show")
-        showing[item].year = true
-    }
+        showing[item].y2024 = true
+    } 
+    if (year === 2023 && showing[item].y2023) {
+        items[item].y2023.classList.replace("show", "hide")
+        showing[item].y2023 = false
+    } else if (year === 2023) {
+        items[item].y2023.classList.replace("hide", "show")
+        showing[item].y2023 = true
+    } 
 }
