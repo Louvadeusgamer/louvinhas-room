ddca467310e63905b19c0e828700bdefc8c22334
LouvinhaUwU <103537728+Louvadeusgamer@users.noreply.github.com>
2024-09-12 19:28:54 -0300

    a

--- colapseScript.js
+++ colapseScript.js
@@ -11,26 +11,31 @@ let showing = {
     }
 }
 
+const i0y2024 = document.getElementById("0-2024")
+const i0y2023 = document.getElementById("0-2023")
+let Si0y2024 = true
+let Si0y2023 = false
 
 
 function colapse(item, year) {
-    console.log("test" + items[0].y2024);
-    if (year === 2024){
-        if (showing[item].y2024) {
-            items[item].y2024.classList.replace("show", "hide")
-            showing[item].y2024 = false
-        } else {
-            items[item].y2024.classList.replace("hide", "show")
-            showing[item].y2024 = true
+    if (item === 0) {
+        if (year === 2024){
+            if (Si0y2024) {
+                i0y2024.classList.replace("show", "hide")
+                Si0y2024 = false
+            } else {
+                i0y2024.classList.replace("hide", "show")
+                Si0y2024 = true
+            } 
+        }
+        if (year === 2023) {
+            if (Si0y2023) {
+                i0y2023.classList.replace("show", "hide")
+                Si0y2023 = false
+            } else {
+                i0y2023.classList.replace("hide", "show")
+                Si0y2023 = true
+            }
         } 
     }
-    if (year === 2023) {
-        if (showing[item].y2023 === true) {
-            items[item].y2023.classList.replace("show", "hide")
-            showing[item].y2023 = false
-        } else {
-            items[item].y2023.classList.replace("hide", "show")
-            showing[item].y2023 = true
-        }
-    } 
 }
