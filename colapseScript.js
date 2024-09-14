1c12dfc9286907e2678f29620b67aa8a5a4acd79
LouvinhaUwU <103537728+Louvadeusgamer@users.noreply.github.com>
2024-09-13 23:11:27 -0300

    More arts on en-us/arts.html

--- colapseScript.js
+++ colapseScript.js
@@ -1,20 +1,12 @@
-const items = {
-    i0: {
-        y2024: document.getElementById("0-2024"),
-        y2023: document.getElementById("0-2023")
-    }
-}
-let showing = {
-    i0: {
-        y2024: true, 
-        y2023: false
-    }
-}
-
 const i0y2024 = document.getElementById("0-2024")
 const i0y2023 = document.getElementById("0-2023")
+const i2y2024 = document.getElementById("2-2024")
+const i2y2023 = document.getElementById("2-2023")
+
 let Si0y2024 = true
 let Si0y2023 = false
+let Si2y2024 = true
+let Si2y2023 = false
 
 
 function colapse(item, year) {
@@ -36,6 +28,27 @@ function colapse(item, year) {
                 i0y2023.classList.replace("hide", "show")
                 Si0y2023 = true
             }
-        } 
+        }
+    }
+
+    if (item === 2) {
+        if (year === 2024) {
+            if (Si2y2024) {
+                i2y2024.classList.replace("show", "hide")
+                Si2y2024 = false 
+            } else {
+                i2y2024.classList.replace("hide", "show")
+                Si2y2024 = true
+            }
+        }
+        if (year === 2023) {
+            if (Si2y2023) {
+                i2y2023.classList.replace("show", "hide")
+                Si2y2023 = false 
+            } else {
+                i2y2023.classList.replace("hide", "show")
+                Si2y2023 = true
+            }
+        }
     }
 }
